d5426c61fe5fe5f9ce92c773c64cb350250b3b8c
Chris <christophergutama123@gmail.com>
2025-06-20 14:47:47 -0400

    added scrolling effects to buttons for trending row

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-what
+const trendingRow = document.getElementById('trendingRow');
+const leftBtn = document.querySelector('.leftBTN');
+const rightBtn = document.querySelector('.rightBTN');
+
+const scrollAmount = 650; 
+
+leftBtn.addEventListener('click', () => {
+  trendingRow.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+});
+
+rightBtn.addEventListener('click', () => {
+  trendingRow.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+});
